faa429db85b21a312564db3cec5d54a4965b22c5
vinay sachan <vinay@tappetbox.com>
2024-07-19 21:00:18 +0530

    changes

--- includes/constant.php
+++ includes/constant.php
@@ -5,7 +5,7 @@ define('CONST_MAIL_PASSWORD', 'EmailPassword@123');
 define('CONST_MAIL_FROM', 'contact@livledger.com');
 define('CONST_MAIL_PORT', '465');
 define('CONST_MAIL_FROM_NAME', 'RobinHood Contact');
-//define('CONST_RECEIVE_EMAIL', 'gracebarrows8722@outlook.com');
-define('CONST_RECEIVE_EMAIL', 'vinaysachan@live.in');
+define('CONST_RECEIVE_EMAIL', 'gracebarrows8722@outlook.com');
+//define('CONST_RECEIVE_EMAIL', 'vinaysachan@live.in');
 
 ?>
